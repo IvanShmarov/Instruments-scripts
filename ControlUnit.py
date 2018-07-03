2087c8d35208ab6a845ac6ce03bfa98d0057afbb
Ivan Shmarov <xiv68x@gmail.com>
2018-07-03 16:51:04 +0100

    Update

--- ControlUnit.py
+++ ControlUnit.py
@@ -2,6 +2,7 @@ import tkinter
 import visa
 import threading
 import time
+import math
 
 root = tkinter.Tk()
 root.title("Control unit")
@@ -9,6 +10,9 @@ root.title("Control unit")
 # IMPORTANT VARIABLES
 KeithleyReadyStatus = tkinter.BooleanVar()
 KeithleyReadyStatus.set(False)
+
+MercuryReadyStatus = tkinter.BooleanVar()
+MercuryReadyStatus.set(False)
 
 StartVoltageEntryValue = tkinter.StringVar()
 StartVoltageEntryValue.set("0.0")
@@ -31,12 +35,26 @@ BothWayScan.set(False)
 ScanningStatus = tkinter.BooleanVar()
 ScanningStatus.set(False)
 
+DrawOriginSettings = tkinter.StringVar()
+DrawOriginSettings.set("SW")
+
+DrawVoltageAxisRangeEntryValue = tkinter.StringVar()
+DrawVoltageAxisRangeEntryValue.set("2.0")
+
+DrawCurrentAxisRangeEntryValue = tkinter.StringVar()
+DrawCurrentAxisRangeEntryValue.set("25e-3")
+
 RAW_DATA = ""
+Voltage = []
+Current = []
+delta_time = []
 
 
 # IMPORTANT VARIABLES END
 
 # INSTRUMENTS
+
+ResMan = visa.ResourceManager()
 
 Keithley = None
 
@@ -66,6 +84,7 @@ def FUNC_SWAP_BUTTON(*args):
 def FUNC_KEITHLEY_STATUS_CHANGE(*args):
     global KeithleyReadyStatus
     global KeithleyStatusLabel
+    global ScanButton
     print(args)
     if KeithleyReadyStatus.get():
         KeithleyStatusLabel["text"] = "Ready"
@@ -73,6 +92,20 @@ def FUNC_KEITHLEY_STATUS_CHANGE(*args):
     else:
         KeithleyStatusLabel["text"] = "Not Ready"
         KeithleyStatusLabel["bg"] = "#ff0000"
+        ScanButton["state"] = tkinter.DISABLED
+        ScanButton["text"] = "Keithley not found"
+        ScanButton["bg"] = "#ff0000"
+
+def FUNC_MERCURY_STATUS_CHANGE(*args):
+    global MercuryReadyStatus
+    global MercuryStatusLabel
+    print(args)
+    if MercuryReadyStatus.get():
+        MercuryStatusLabel["text"] = "Ready"
+        MercuryStatusLabel["bg"] = "#00ff00"
+    else:
+        MercuryStatusLabel["text"] = "Not Ready"
+        MercuryStatusLabel["bg"] = "#ff0000"
 
 def FUNC_SCANNING_STATUS_CHANGE(*args):
     global ScanningStatus
@@ -89,18 +122,18 @@ def FUNC_SCANNING_STATUS_CHANGE(*args):
         ScanButton["text"] = "SCAN"
         ScanButton["bg"] = "#00ff00"
 
-def FUNC_CANVAS_INI():
-    global ResultCanvas
-    ResultCanvas.create_line(10,
-                      490,
-                      490,
-                      490,
-                             tags=("axis","yaxis"))
-    ResultCanvas.create_line(10,
-                      10,
-                      10,
-                      490,
-                             tags=("axis","xaxis"))
+def FUNC_REFRESH_INST_BUTTON(*args):
+    FindKeithleyThread = threading.Thread(target=THREAD_FIND_KEITHLEY, name="FINDING KEITHLEY")
+    FindMercuryThread = threading.Thread(target=THREAD_FIND_MERCURY, name="FINDING MERCURY")
+    FindKeithleyThread.start()
+    FindMercuryThread.start()
+    
+
+
+def FUNC_REDRAW_BUTTON():
+    DrawThread = threading.Thread(target=THREAD_DRAW_DATA, name="DRAWING DATA")
+    DrawThread.start()
+    
     
     
 # FUNCTIONS END
@@ -109,9 +142,9 @@ def FUNC_CANVAS_INI():
 def THREAD_FIND_KEITHLEY():
     global Keithley
     global KeithleyReadyStatus
+    global ResMan
 
     # Creating a resource manager
-    ResMan = visa.ResourceManager()
     # Getting the list of all instruments
     list_of_instruments = ResMan.list_resources()
     # Setting keithley initial address to the empty string
@@ -137,6 +170,30 @@ def THREAD_FIND_KEITHLEY():
         print("Keithley was not found")
         KeithleyReadyStatus.set(False)
 
+def THREAD_FIND_MERCURY():
+    global Mercury
+    global MercuryReadyStatus
+    global ResMan
+
+    list_of_instruments = ResMan.list_resources()
+    
+    for item in list_of_instruments:
+        if ("ASRL" in item.upper()):
+            try:
+                temp = ResMan.open_resource(item)
+                if ("MERCURY" in temp.query("*IDN?")):
+                    Mercury = temp
+                    del temp
+                    Mercury.timeout = 60000
+                    MercuryReadyStatus.set(True)
+                    break
+            except visa.VisaIOError:
+                pass
+    if Mercury:
+        print("Found Mercury: ", Mercury.query("*IDN?"))
+    else:
+        print("Mercury was not found")
+        MercuryReadyStatus.set(False)
 
 def THREAD_SCAN():
     global Keithley
@@ -182,79 +239,112 @@ SENS:FUNC "CURR";
 
 def THREAD_DATA_PROCESSING():
     global RAW_DATA
-    global ResultCanvas
-    global BothWayScan
-    Both = BothWayScan.get()
+    global Voltage
+    global Current
+    global delta_time
     print("Processing Results")
     if RAW_DATA:
         # EXTRACTING DATA
-        V = [float(item) for item in RAW_DATA.split(",")[0::3]]
-        I = [float(item) for item in RAW_DATA.split(",")[1::3]]
-        t = [float(item) for item in RAW_DATA.split(",")[2::3]]
-        # DO SOMETHING WITH DATA
-        print("Time it took: ", t[-1])
-
-        # PLOT DATA
-        ResultCanvas.delete(*ResultCanvas.find_withtag("plot"))
-
-        if max(V) != min(V) and max(I) != min(I):
-    
-            x_points = [10 + (v - min(V))/(max(V) - min(V))*480 for v in V]
-            y_points = [490 - (i - min(I))/(max(I) - min(I))*480 for i in I]
-            ox = 10 - 480*min(V)/(max(V) - min(V))
-            oy = 490 + 480*min(I)/(max(I) - min(I))
-
-            if Both:
-                half_len = round(len(x_points)/2)
-                ResultCanvas.create_line(list(zip(x_points[:half_len+1],y_points[:half_len+1])),
-                                         tags="plot",
-                                         fill="#0000ff")
-                ResultCanvas.create_line(list(zip(x_points[half_len:],y_points[half_len:])),
-                                         tags="plot",
-                                         fill="#00ff00")
-            else: 
-                ResultCanvas.create_line(list(zip(x_points,y_points)),
-                                         tags="plot",
-                                         fill="#0000ff")
-
-            ResultCanvas.create_line(10,oy,490,oy,
-                                    fill="#ff0000",
-                                    tags="plot",
-                                    dash=(10,5))
-            ResultCanvas.create_line(ox,10,ox,490,
-                                    fill="#ff0000",
-                                    tags="plot",
-                                    dash=(5,10))
-
-            
-            
-            ResultCanvas.create_text(15,490, text=str(V[0]), tags="plot",
-                                     anchor = tkinter.NW)
-            ResultCanvas.create_text(440,490, text=str(V[-1]), tags="plot",
-                                     anchor = tkinter.NW)
-
-            ResultCanvas.create_text(15,470, text=str(I[0]), tags="plot",
-                                     anchor = tkinter.NW)
-            ResultCanvas.create_text(15,10, text=str(I[-1]), tags="plot",
-                                     anchor = tkinter.NW)
-            
+        Voltage = [float(item) for item in RAW_DATA.split(",")[0::3]]
+        Current = [float(item) for item in RAW_DATA.split(",")[1::3]]
+        delta_time = [float(item) for item in RAW_DATA.split(",")[2::3]]
         print("Results are processed")
+    FUNC_REDRAW_BUTTON()
+
+def THREAD_DRAW_DATA():
+    global ResultCanvas
+    global Voltage
+    global Current
+    global DrawOriginSettings
+    global DrawVoltageAxisRangeEntryValue
+    global DrawCurrentAxisRangeEntryValue
+    
+    ResultCanvas.delete(*ResultCanvas.find_withtag("plot"))
+    canvas_width = float(ResultCanvas["width"])
+    canvas_height = float(ResultCanvas["height"])
+    x_origin = canvas_width/2;
+    y_origin = canvas_height/2;
+    x_range = abs(float(DrawVoltageAxisRangeEntryValue.get()))
+    y_range = abs(float(DrawCurrentAxisRangeEntryValue.get()))
+
+    if "N" in DrawOriginSettings.get().upper():
+        y_origin -= 240
+    elif "S" in DrawOriginSettings.get().upper():
+        y_origin += 240
+
+    if "W" in DrawOriginSettings.get().upper():
+        x_origin -= 240
+    elif "E" in DrawOriginSettings.get().upper():
+        x_origin += 240
+
+    ResultCanvas.create_line(10, y_origin, canvas_width-10, y_origin,
+                             fill="#010101",
+                             tags=("plot", "axis"))
+
+    ResultCanvas.create_line(x_origin, 10, x_origin, canvas_height-10,
+                             fill="#010101",
+                             tags=("plot", "axis"))
+
+    dx = 0.25 * math.pow( 10, math.floor(math.log10(x_range)) )
+    dy = 0.25 * math.pow( 10, math.floor(math.log10(y_range)) )
+
+    for i in range(1, math.floor(x_range/dx)+1):
+        ResultCanvas.create_line((dx*i)/x_range*(canvas_width-20)+x_origin, 10, (dx*i)/x_range*(canvas_width-20)+x_origin, canvas_height-10,
+                                 fill="#aaaaaa", tags=("plot","axis"))
+        ResultCanvas.create_text((dx*i)/x_range*(canvas_width-20)+x_origin, canvas_height-10, anchor=tkinter.NW, text="%2.2e"%(dx*i), tags=("plot", "axis"))
+        
+        ResultCanvas.create_line(-(dx*i)/x_range*(canvas_width-20)+x_origin, 10, -(dx*i)/x_range*(canvas_width-20)+x_origin, canvas_height-10,
+                                 fill="#aaaaaa", tags=("plot","axis"))
+        ResultCanvas.create_text(-(dx*i)/x_range*(canvas_width-20)+x_origin, canvas_height-10, anchor=tkinter.NW, text="%2.2e"%(-dx*i), tags=("plot", "axis"))
+
+    for j in range(1, math.floor(y_range/dy)+1):
+        ResultCanvas.create_line( 10, -(dy*j)/y_range*(canvas_width-20)+y_origin, canvas_width-10, -(dy*j)/y_range*(canvas_width-20)+y_origin,
+                                 fill="#aaaaaa", tags=("plot","axis"))
+        ResultCanvas.create_text(10, -(dy*j)/y_range*(canvas_width-20)+y_origin, anchor=tkinter.NW, text="%2.2e"%(dy*j), tags=("plot", "axis"))
+        
+        ResultCanvas.create_line( 10, (dy*j)/y_range*(canvas_width-20)+y_origin, canvas_width-10, (dy*j)/y_range*(canvas_width-20)+y_origin,
+                                 fill="#aaaaaa", tags=("plot","axis"))
+        ResultCanvas.create_text(10, (dy*j)/y_range*(canvas_width-20)+y_origin, anchor=tkinter.NW, text="%2.2e"%(-dy*j), tags=("plot", "axis"))
+
+    if Voltage and Current:
+        x_points = [v/x_range * (canvas_width-20) + x_origin for v in Voltage]
+        y_points = [-i/y_range * (canvas_height-20) + y_origin for i in Current]
+
+        ResultCanvas.create_line(list(zip(x_points, y_points)),
+                                 fill="#1010ff", width=2,
+                                 tags=("plot"))
+        
+    
+    pass
 
 # THREADS END
+
+# THREADS INI
+
+
+# THREADS INI END
 
 # FRAMES
 StatusFrame = tkinter.LabelFrame(root)
 StatusFrame["text"] = "Status"
 
 ControlFrame = tkinter.LabelFrame(root)
-ControlFrame["text"] = "Control"
+ControlFrame["text"] = "Keithley Sweep Control"
 
 ResultFrame = tkinter.LabelFrame(root)
 ResultFrame["text"] = "Result"
 
+MercFrame = tkinter.LabelFrame(root)
+MercFrame["text"] = "Mercury Control"
+
+SaveFrame = tkinter.LabelFrame(root)
+SaveFrame["text"] = "Save data"
+
 StatusFrame.grid(row=0,column=0)
 ControlFrame.grid(row=1,column=0)
 ResultFrame.grid(row=0,column=1,rowspan=2)
+MercFrame.grid(row=0,column=2)
+SaveFrame.grid(row=1,column=2)
 # FRAMES END
 
 # STATUS FRAME
@@ -264,9 +354,14 @@ KeithleyStatusLabel.grid(row=0,column=1)
 KeithleyReadyStatus.trace("w", FUNC_KEITHLEY_STATUS_CHANGE)
 
 tkinter.Label(StatusFrame, text="Mercury: ").grid(row=1,column=0)
-MercuryStatusLabel = tkinter.Label(StatusFrame, text="Not supported Yet",
-                                   bg="#ffff00")
+MercuryStatusLabel = tkinter.Label(StatusFrame, text="Pending",
+                                   bg="#ff0000")
 MercuryStatusLabel.grid(row=1,column=1)
+MercuryReadyStatus.trace("w", FUNC_MERCURY_STATUS_CHANGE)
+
+RefreshInstrumentsButton = tkinter.Button(StatusFrame, text="Refresh Instruments",
+                                          command=FUNC_REFRESH_INST_BUTTON)
+RefreshInstrumentsButton.grid(row=2,column=0,columnspan=2)
 # STATUS FRAME END
 
 # CONTROL FRAME
@@ -316,14 +411,76 @@ ResultCanvas = tkinter.Canvas(ResultFrame)
 ResultCanvas["width"] = 500
 ResultCanvas["height"] = 500
 ResultCanvas["bg"] = "white"
-ResultCanvas.pack()
+ResultCanvas.grid(row=0,column=0,columnspan=2)
+
+OriginSettingsFrame = tkinter.LabelFrame(ResultFrame, text="Origin")
+OriginSettingsFrame.grid(row=1,column=0)
+
+OriginNW = tkinter.Radiobutton(OriginSettingsFrame, variable=DrawOriginSettings,
+                              value="NW")
+OriginNW.grid(row=0,column=0)
+
+OriginN = tkinter.Radiobutton(OriginSettingsFrame, variable=DrawOriginSettings,
+                              value="N")
+OriginN.grid(row=0,column=1)
+
+OriginNE = tkinter.Radiobutton(OriginSettingsFrame, variable=DrawOriginSettings,
+                              value="NE")
+OriginNE.grid(row=0,column=2)
+
+OriginW = tkinter.Radiobutton(OriginSettingsFrame, variable=DrawOriginSettings,
+                              value="W")
+OriginW.grid(row=1,column=0)
+
+OriginC = tkinter.Radiobutton(OriginSettingsFrame, variable=DrawOriginSettings,
+                              value="C")
+OriginC.grid(row=1,column=1)
+
+OriginE = tkinter.Radiobutton(OriginSettingsFrame, variable=DrawOriginSettings,
+                              value="E")
+OriginE.grid(row=1,column=2)
+
+OriginSW = tkinter.Radiobutton(OriginSettingsFrame, variable=DrawOriginSettings,
+                              value="SW")
+OriginSW.grid(row=2,column=0)
+
+OriginS = tkinter.Radiobutton(OriginSettingsFrame, variable=DrawOriginSettings,
+                              value="S")
+OriginS.grid(row=2,column=1)
+
+OriginSE = tkinter.Radiobutton(OriginSettingsFrame, variable=DrawOriginSettings,
+                              value="SE")
+OriginSE.grid(row=2,column=2)
+
+AxisSettingsFrame = tkinter.LabelFrame(ResultFrame, text="Axis")
+AxisSettingsFrame.grid(row=1,column=1)
+
+tkinter.Label(AxisSettingsFrame, text="Voltage range: ").grid(row=0,column=0)
+DrawVoltageAxisSettingsEntry = tkinter.Entry(AxisSettingsFrame,
+                                             textvariable=DrawVoltageAxisRangeEntryValue)
+DrawVoltageAxisSettingsEntry.grid(row=0,column=1)
+
+tkinter.Label(AxisSettingsFrame, text="Current range: ").grid(row=1,column=0)
+DrawCurrentAxisSettingsEntry = tkinter.Entry(AxisSettingsFrame,
+                                             textvariable=DrawCurrentAxisRangeEntryValue)
+DrawCurrentAxisSettingsEntry.grid(row=1,column=1)
+
+RedrawButton = tkinter.Button(ResultFrame, text="Redraw",
+                              command=FUNC_REDRAW_BUTTON)
+RedrawButton.grid(row=2,column=0,columnspan=2)
+
 # RESULT FRAME END
 
-# INI AREA
-FindKeithleyThread = threading.Thread(target=THREAD_FIND_KEITHLEY, name="FINDING KEITHLEY")
-FindKeithleyThread.start()
+# MERCURY FRAME
+# MERCURY FRAME END
 
-FUNC_CANVAS_INI()
+# SAVE DATA FRAME
+# SAVE DATA FRAME END
+
+# INI AREA
+FUNC_REFRESH_INST_BUTTON()
+
+FUNC_REDRAW_BUTTON()
 
 ScanningStatus.trace("w", FUNC_SCANNING_STATUS_CHANGE)
 
