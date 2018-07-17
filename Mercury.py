215534679408bbc90dd2ba67b1618e98a48bbaaf
Ivan Shmarov <xiv68x@gmail.com>
2018-07-17 15:37:30 +0100

    update

--- Mercury.py
+++ Mercury.py
@@ -18,8 +18,12 @@ for item in list_of_instruments:
             print("Not a thing")
 if Mercury:
     print("Found Mercury: ", Mercury.query("*IDN?"))
+    print(Mercury.query("SET:DEV:MB1.T1:TEMP:LOOP:ENAB:ON"))
+    print(Mercury.query("SET:DEV:MB1.T1:TEMP:LOOP:PIDT:ON"))
+    print(Mercury.query("SET:DEV:MB1.T1:TEMP:LOOP:TSET:60"))
 else:
     print("Mercury was not found")
+    
 #SET:DEV:MB1.T1:TEMP:LOOP:ENAB:ON
 #SET:DEV:MB1.T1:TEMP:LOOP:PIDT:ON
 #SET:DEV:MB1.T1:TEMP:LOOP:TSET:X
